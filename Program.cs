c5b20307fb0a092caf2843c01ed8a0a5afa0c52e
Viktor Vasylkovskyi <viktor.vasylkovskyi@sky.uk>
2020-09-29 00:01:14 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DIDA_GSTORE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

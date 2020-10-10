db1240dd6e23197d2d7c673e3886c8a4551724f4
Viktor Vasylkovskyi <viktor.vasylkovskyi@sky.uk>
2020-10-10 20:02:20 +0100

    Puppet Master and PCS. StartServer and StartClient commands implementation

---
+++ processCreationService/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace processCreationService
+{
+    public class Program : IProcessCreationService
+    {
+        List<Process> processesList = new List<Process>();
+        static void Main(string[] args)
+        {
+            new Program().Init(args);
+        }
+
+        private void Init(string[] args)
+        {
+
+        }
+
+        public void StartServer(string args)
+        {
+            Console.WriteLine(">>> Starting Server: " + args);
+            processesList.Add(Process.Start("../server/bin/Debug/netcoreapp3.1/server.dll", args));
+        }
+
+        public void StartClient(string args)
+        {
+            Console.WriteLine(">>> Starting Client: " + args);
+            processesList.Add(Process.Start("../client/bin/Debug/netcoreapp3.1/client.dll", args));
+        }
+
+        public void ShutdownAllProcesses()
+        {
+            Console.WriteLine(">>> Exiting all processes");
+            foreach (Process process in processesList)
+            {
+                try
+                {
+                    process.CloseMainWindow();
+                    process.Close();
+                }
+                catch (InvalidOperationException)
+                {
+                    Console.WriteLine(">>> Exception, Process is already closed");
+                }
+            }
+            Console.WriteLine(">>> Done.");
+        }
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Shared;
+
+namespace server
+{
+    class Program : IServerClientCommands
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public void getStatus()
+        {
+            Console.WriteLine(">>> Status");
+        }
+    }
+}
